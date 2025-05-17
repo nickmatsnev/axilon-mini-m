b0b39f9e94d4b1d1a28fdc81e05f21ae13f7a75c
nickmatsnev <matsnev2001@yandex.ru>
2025-05-17 13:24:19 +0200

    neue bau, jetzt kein fcm

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -356,14 +356,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f7304e92ffc1043a150e5bc9570f5226
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "66e5e03fd385fbbeb13797d2672f0d80",
+"flutter_bootstrap.js": "ab1d58bd80b7c7bf026f6b80161e180c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1d4e3d61026f5af08318fa398fcd7244",
 "/": "1d4e3d61026f5af08318fa398fcd7244",
-"main.dart.js": "d56ac29c051e8116c217065c56338b51",
+"main.dart.js": "49d13d7c81a612d0370c1d8b6d20da1d",
 "manifest.json": "af6939727b44242d915ee84ba59cbca9",
 "version.json": "d7350f72fa1ebcf31761cdfbfbee4456"};
 // The application shell files that are downloaded before a service worker can
--- lib/main.dart
+++ lib/main.dart
@@ -30,100 +30,12 @@ import 'pages/ussd_setup_page.dart';
 // Other Utils
 import 'utils/call_listener.dart';
 
-// Firebase
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'firebase_options.dart';
 
 // HTTP
 import 'package:http/http.dart' as http;
 
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  print('Handling a background message: ${message.messageId}');
-}
-
-Future<void> initFCM(BuildContext context) async {
-  // 1) Request permissions (iOS, web, Android 13+)
-  final settings = await FirebaseMessaging.instance.requestPermission(
-    alert: true,
-    badge: true,
-    sound: true,
-    provisional: false,
-  );
-  print('User granted permission: ${settings.authorizationStatus}');
-
-  // 2) Get the token
-  String? token = await FirebaseMessaging.instance.getToken();
-  print('FCM token: $token');
-
-  if (token != null) {
-    // 3) Send token to your backend
-    final authProvider = Provider.of<AuthProvider>(context, listen: false);
-
-    if (authProvider.token != null) {
-      try {
-        final bearerToken = authProvider.token;
-        final response = await http.post(
-          Uri.parse('https://axilon-mini-be-e5732e59dadc.herokuapp.com/api/users/set-fcm-token'),
-          headers: {
-            'Authorization': 'Bearer $bearerToken',
-            'Content-Type': 'application/json',
-          },
-          body: json.encode({'fcm_token': token, 'userId': authProvider.user?['user_id']}),
-        );
-        if (authProvider.user?['user_id'] != null){
-          NotificationService().initialize(authProvider.user?['user_id']);
-        }
-        if (response.statusCode == 200) {
-          print("FCM token stored on server");
-        } else {
-          print("Error storing FCM token: ${response.statusCode} => ${response.body}");
-        }
-      } catch (e) {
-        print("Exception while storing FCM token: $e");
-      }
-    }
-  }
-
-  // 4) Listen for token refresh
-  FirebaseMessaging.instance.onTokenRefresh.listen((newToken) async {
-    print('FCM token refreshed: $newToken');
-    final authProvider = Provider.of<AuthProvider>(context, listen: false);
-    if (authProvider.token != null) {
-      try {
-        final bearerToken = authProvider.token;
-        await http.post(
-          Uri.parse('https://axilon-mini-be-e5732e59dadc.herokuapp.com/api/users/set-fcm-token'),
-          headers: {
-            'Authorization': 'Bearer $bearerToken',
-            'Content-Type': 'application/json',
-          },
-          body: json.encode({'fcm_token': newToken, 'userId': authProvider.user?['user_id']}),
-        );
-      } catch (e) {
-        print("Error refreshing FCM token: $e");
-      }
-    }
-  });
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  // Set the background message handler early
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  // Example: Listen for messages in the foreground
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Got a message in foreground: ${message.notification?.title}, ${message.notification?.body}');
-  });
 
   await PermissionsService().requestAllPermissions();
 
@@ -161,39 +73,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    // Use a post-frame callback to ensure context is available
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      initFCM(context);
-    });
-
-  }
 
   @override
   Widget build(BuildContext context) {
     // Start listening for phone calls
     CallListener.startListening(context);
-
-    void handleMessage(RemoteMessage message) {
-      if (message.data['type'] == 'task') {
-        Navigator.pushNamed(context, '/scenarios');
-      } else if (message.data['type'] == 'call') {
-        Navigator.pushNamed(context, '/logs');
-      }
-    }
-
-    Future<void> setupInteractedMessage() async {
-      RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
-      if (initialMessage != null) {
-        handleMessage(initialMessage);
-      }
-      FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
-    }
-
-    // Optionally, handle any initial interaction message
-    setupInteractedMessage();
 
     return MaterialApp(
       title: 'Axilon',
