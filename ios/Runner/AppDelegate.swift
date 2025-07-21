54838caa07224c0da319dfd614e8996a48e3e040
nickmatsnev <matsnev2001@yandex.ru>
2025-07-21 08:44:37 +0200

    fix apns

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,34 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 1) Configure Firebase
+    FirebaseApp.configure()
+
+    // 2) Set UNUserNotificationCenter delegate for foreground notifications
+    UNUserNotificationCenter.current().delegate = self
+
+    // 3) Register for remote notifications
+    application.registerForRemoteNotifications()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // 4) This is called when APNs has generated a device token for you
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    // Pass it to Firebase Messaging
+    Messaging.messaging().apnsToken = deviceToken
+
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
